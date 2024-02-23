c6122e88807443b1a631fe9db5723f3e03163b50
diegoRrocha221 <diego.adm.rocha@gmail.com>
2024-02-23 06:00:53 -0300

    add skeleton jointEvent method

--- src/main/java/EventSystemAnhembi.java
+++ src/main/java/EventSystemAnhembi.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 public class EventSystemAnhembi {
 	private List<User> users;
+	private List<Event> events;
 	
     public EventSystemAnhembi() {
         this.users = new ArrayList<>();
+        this.events = new ArrayList<>();
     }
 	
     public void registerUser(String name, String city, String email) {
@@ -15,6 +17,13 @@ public class EventSystemAnhembi {
         users.add(user);
     }
     
+    public void joinEvent(User user, Event event) {
+        if (events.contains(event)) {
+            System.out.println(user.name + " participou do evento: " + event.name);
+        } else {
+            System.out.println("Evento não encontrado.");
+        }
+    }
     
     public List<User> getUsers() {
         return users;
